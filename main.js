68b13195b14e47654e696c20a7688f4320aae628
margilan <shermatovelbek88@gmail.com>
2023-12-27 15:52:09 +0500

    uyga vazifa tigatildi uzatishga tayyot holatda

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
-const button = document.querySelector('#btn');
-const block = document.querySelector('#block');
+// const button = document.querySelector('#btn');
+// const block = document.querySelector('#block');
 
-btn.style.width = "100px"
-btn.style.height = "40px"
-btn.style.cursor = "pointer";
-btn.style.border = "none"
-btn.style.marginTop = "200px"
-btn.style.marginLeft = "1200px"
+// btn.style.width = "100px"
+// btn.style.height = "40px"
+// btn.style.cursor = "pointer";
+// btn.style.border = "none"
+// btn.style.marginTop = "200px"
+// btn.style.marginLeft = "1200px"
 
-button.addEventListener('click' , function(){
-    const img = document.createElement('img');
-    img.setAttribute('alt' , "bu yerda rasm bor")
-    img.setAttribute("src" , 'https://picsum.photos/seed/picsum/200/200');
-    img.style.marginLeft = "60px";
-
-    block.style.border = "1px solid black";
-    block.style.padding = "50px";
-    block.appendChild(img);
-    console.log(img)
-})
 // button.addEventListener('click' , function(){
 //     const img = document.createElement('img');
 //     img.setAttribute('alt' , "bu yerda rasm bor")
-//     img.setAttribute("src" , 'https://picsum.photos/200/200?grayscale');
-//     alert = "bu yerga kirmoqchimisiz"
+//     img.setAttribute("src" , 'https://picsum.photos/seed/picsum/200/200');
 //     img.style.marginLeft = "60px";
 
 //     block.style.border = "1px solid black";
@@ -31,5 +19,20 @@ button.addEventListener('click' , function(){
 //     block.appendChild(img);
 //     console.log(img)
 // })
+// // button.addEventListener('click' , function(){
+// //     const img = document.createElement('img');
+// //     img.setAttribute('alt' , "bu yerda rasm bor")
+// //     img.setAttribute("src" , 'https://picsum.photos/200/200?grayscale');
+// //     alert = "bu yerga kirmoqchimisiz"
+// //     img.style.marginLeft = "60px";
+
+// //     block.style.border = "1px solid black";
+// //     block.style.padding = "50px";
+// //     block.appendChild(img);
+// //     console.log(img)
+// // })
+
+
+//uyga vazifa tugatildi
 
 
